f9b352295b158a29dc0543fb66e87487351dcb42
Rory Eastham <itzrozzadev@gmail.com>
2021-05-30 23:45:22 +0100

    Update SimpleDatabase
    - Synchronize batch update

--- src/main/java/com/itzrozzadev/fo/database/SimpleDatabase.java
+++ src/main/java/com/itzrozzadev/fo/database/SimpleDatabase.java
@@ -17,14 +17,12 @@ import java.util.Map.Entry;
 import java.util.Timer;
 import java.util.TimerTask;
 
+
 /**
  * Represents a simple MySQL database
- * <p>
  * Before running queries make sure to call connect() methods.
- * <p>
  * You can also override {@link #onConnected()} to run your code after the
  * connection has been established.
- * <p>
  * To use this class you must know the MySQL command syntax!
  */
 public class SimpleDatabase {
@@ -159,9 +157,9 @@ public class SimpleDatabase {
 	 * Attempts to connect using last known credentials. Fails gracefully if those are not provided
 	 * i.e. connect function was never called
 	 */
-	private void connectUsingLastCredentials() {
-		if (lastCredentials != null)
-			connect(lastCredentials.url, lastCredentials.user, lastCredentials.password, lastCredentials.table);
+	private final void connectUsingLastCredentials() {
+		if (this.lastCredentials != null)
+			connect(this.lastCredentials.url, this.lastCredentials.user, this.lastCredentials.password, this.lastCredentials.table);
 	}
 
 	/**
@@ -178,10 +176,10 @@ public class SimpleDatabase {
 	 * Attempts to close the connection, if not null
 	 */
 	public final void close() {
-		if (connection != null)
-			synchronized (connection) {
+		if (this.connection != null)
+			synchronized (this.connection) {
 				try {
-					connection.close();
+					this.connection.close();
 
 				} catch (final SQLException e) {
 					Common.error(e, "Error closing MySQL connection!");
@@ -206,12 +204,12 @@ public class SimpleDatabase {
 	 * Insert the given column-values pairs into the given table
 	 *
 	 * @param table
-	 * @param columsAndValues
+	 * @param map
 	 */
-	protected final void insert(final String table, @NonNull final SerializedMap columsAndValues) {
-		final String columns = Common.join(columsAndValues.keySet());
-		final String values = Common.join(columsAndValues.values(), ", ", value -> value == null || value.equals("NULL") ? "NULL" : "'" + SerializeUtil.serialize(value).toString() + "'");
-		final String duplicateUpdate = Common.join(columsAndValues.entrySet(), ", ", entry -> entry.getKey() + "=VALUES(" + entry.getKey() + ")");
+	protected final void insert(final String table, @NonNull final SerializedMap map) {
+		final String columns = Common.join(map.keySet());
+		final String values = Common.join(map.values(), ", ", value -> value == null || value.equals("NULL") ? "NULL" : "'" + SerializeUtil.serialize(value).toString() + "'");
+		final String duplicateUpdate = Common.join(map.entrySet(), ", ", entry -> entry.getKey() + "=VALUES(" + entry.getKey() + ")");
 
 		update("INSERT INTO " + replaceVariables(table) + " (" + columns + ") VALUES (" + values + ") ON DUPLICATE KEY UPDATE " + duplicateUpdate + ";");
 	}
@@ -236,7 +234,7 @@ public class SimpleDatabase {
 
 		for (final SerializedMap map : maps) {
 			final String columns = Common.join(map.keySet());
-			final String values = Common.join(map.values(), ", ", value -> parseValue(value));
+			final String values = Common.join(map.values(), ", ", this::parseValue);
 			final String duplicateUpdate = Common.join(map.entrySet(), ", ", entry -> entry.getKey() + "=VALUES(" + entry.getKey() + ")");
 
 			sqls.add("INSERT INTO " + table + " (" + columns + ") VALUES (" + values + ") ON DUPLICATE KEY UPDATE " + duplicateUpdate + ";");
@@ -248,7 +246,7 @@ public class SimpleDatabase {
 	/*
 	 * A helper method to insert compatible value to db
 	 */
-	private String parseValue(final Object value) {
+	private final String parseValue(final Object value) {
 		return value == null || value.equals("NULL") ? "NULL" : "'" + SerializeUtil.serialize(value).toString() + "'";
 	}
 
@@ -262,7 +260,7 @@ public class SimpleDatabase {
 	protected final void update(String sql) {
 		checkEstablished();
 
-		synchronized (connection) {
+		synchronized (this.connection) {
 			if (!isConnected())
 				connectUsingLastCredentials();
 
@@ -272,7 +270,7 @@ public class SimpleDatabase {
 			Debugger.debug("mysql", "Updating MySQL with: " + sql);
 
 			try {
-				final Statement statement = connection.createStatement();
+				final Statement statement = this.connection.createStatement();
 
 				statement.executeUpdate(sql);
 				statement.close();
@@ -294,7 +292,7 @@ public class SimpleDatabase {
 	protected final ResultSet query(String sql) {
 		checkEstablished();
 
-		synchronized (connection) {
+		synchronized (this.connection) {
 			if (!isConnected())
 				connectUsingLastCredentials();
 
@@ -303,7 +301,7 @@ public class SimpleDatabase {
 			Debugger.debug("mysql", "Querying MySQL with: " + sql);
 
 			try {
-				final Statement statement = connection.createStatement();
+				final Statement statement = this.connection.createStatement();
 				final ResultSet resultSet = statement.executeQuery(sql);
 
 				return resultSet;
@@ -327,56 +325,58 @@ public class SimpleDatabase {
 		if (sqls.size() == 0)
 			return;
 
-		try {
-			final Statement batchStatement = getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
-			final int processedCount = sqls.size();
-
-			// Prevent automatically sending db instructions
-			getConnection().setAutoCommit(false);
-
-			for (final String sql : sqls)
-				batchStatement.addBatch(replaceVariables(sql));
-
-			if (processedCount > 10_000)
-				Common.log("Updating your database (" + processedCount + " entries)... PLEASE BE PATIENT THIS WILL TAKE "
-						+ (processedCount > 50_000 ? "10-20 MINUTES" : "5-10 MINUTES") + " - If server will print a crash report, ignore it, update will proceed.");
-
-			// Set the flag to start time notifications timer
-			batchUpdateGoingOn = true;
-
-			// Notify console that progress still is being made
-			new Timer().scheduleAtFixedRate(new TimerTask() {
-
-				@Override
-				public void run() {
-					if (batchUpdateGoingOn)
-						Common.log("Still executing, " + RandomUtil.nextItem("keep calm", "stand by", "watch the show", "check your db", "drink water", "call your friend") + " and DO NOT SHUTDOWN YOUR SERVER.");
-					else
-						cancel();
-				}
-			}, 1000 * 30, 1000 * 30);
-
-			// Execute
-			batchStatement.executeBatch();
-
-			// This will block the thread
-			getConnection().commit();
-
-			//Common.log("Updated " + processedCount + " database entries.");
-
-		} catch (final Throwable t) {
-			t.printStackTrace();
-
-		} finally {
+		synchronized (this.connection) {
 			try {
-				getConnection().setAutoCommit(true);
+				final Statement batchStatement = getConnection().createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_UPDATABLE);
+				final int processedCount = sqls.size();
 
-			} catch (final SQLException ex) {
-				ex.printStackTrace();
+				// Prevent automatically sending db instructions
+				getConnection().setAutoCommit(false);
+
+				for (final String sql : sqls)
+					batchStatement.addBatch(replaceVariables(sql));
+
+				if (processedCount > 10_000)
+					Common.log("Updating your database (" + processedCount + " entries)... PLEASE BE PATIENT THIS WILL TAKE "
+							+ (processedCount > 50_000 ? "10-20 MINUTES" : "5-10 MINUTES") + " - If server will print a crash report, ignore it, update will proceed.");
+
+				// Set the flag to start time notifications timer
+				this.batchUpdateGoingOn = true;
+
+				// Notify console that progress still is being made
+				new Timer().scheduleAtFixedRate(new TimerTask() {
+
+					@Override
+					public void run() {
+						if (SimpleDatabase.this.batchUpdateGoingOn)
+							Common.log("Still executing, " + RandomUtil.nextItem("keep calm", "stand by", "watch the show", "check your db", "drink water", "call your friend") + " and DO NOT SHUTDOWN YOUR SERVER.");
+						else
+							cancel();
+					}
+				}, 1000 * 30, 1000 * 30);
+
+				// Execute
+				batchStatement.executeBatch();
+
+				// This will block the thread
+				getConnection().commit();
+
+				//Common.log("Updated " + processedCount + " database entries.");
+
+			} catch (final Throwable t) {
+				t.printStackTrace();
+
+			} finally {
+				try {
+					getConnection().setAutoCommit(true);
+
+				} catch (final SQLException ex) {
+					ex.printStackTrace();
+				}
+
+				// Even in case of failure, cancel
+				this.batchUpdateGoingOn = false;
 			}
-
-			// Even in case of failure, cancel
-			batchUpdateGoingOn = false;
 		}
 	}
 
@@ -392,7 +392,7 @@ public class SimpleDatabase {
 	protected final java.sql.PreparedStatement prepareStatement(String sql) throws SQLException {
 		checkEstablished();
 
-		synchronized (connection) {
+		synchronized (this.connection) {
 			if (!isConnected())
 				connectUsingLastCredentials();
 
@@ -400,7 +400,7 @@ public class SimpleDatabase {
 
 			Debugger.debug("mysql", "Preparing statement: " + sql);
 
-			return connection.prepareStatement(sql);
+			return this.connection.prepareStatement(sql);
 		}
 	}
 
@@ -414,9 +414,9 @@ public class SimpleDatabase {
 		if (!isLoaded())
 			return false;
 
-		synchronized (connection) {
+		synchronized (this.connection) {
 			try {
-				return connection != null && !connection.isClosed() && connection.isValid(0);
+				return this.connection != null && !this.connection.isClosed() && this.connection.isValid(0);
 
 			} catch (final SQLException ex) {
 				return false;
@@ -436,13 +436,13 @@ public class SimpleDatabase {
 	protected final String getTable() {
 		checkEstablished();
 
-		return Common.getOrEmpty(lastCredentials.table);
+		return Common.getOrEmpty(this.lastCredentials.table);
 	}
 
 	/**
 	 * Checks if the connect() function was called
 	 */
-	private void checkEstablished() {
+	private final void checkEstablished() {
 		Valid.checkBoolean(isLoaded(), "Connection was never established");
 	}
 
@@ -452,7 +452,7 @@ public class SimpleDatabase {
 	 * @return
 	 */
 	public final boolean isLoaded() {
-		return connection != null;
+		return this.connection != null;
 	}
 
 	// --------------------------------------------------------------------
@@ -467,7 +467,7 @@ public class SimpleDatabase {
 	 * @param value
 	 */
 	protected final void addVariable(final String name, final String value) {
-		sqlVariables.put(name, value);
+		this.sqlVariables.put(name, value);
 	}
 
 	/**
@@ -478,7 +478,7 @@ public class SimpleDatabase {
 	 */
 	protected final String replaceVariables(String sql) {
 
-		for (final Entry<String, String> entry : sqlVariables.entrySet())
+		for (final Entry<String, String> entry : this.sqlVariables.entrySet())
 			sql = sql.replace("{" + entry.getKey() + "}", entry.getValue());
 
 		return sql.replace("{table}", getTable());
